456a2597c62faa40e97fdd2c2e3844b6d7a7198d
Jonathan Eriksson <LandisGaming@hotmail.com>
2019-05-17 02:37:27 +0200

    Redesigned Smorgasbord

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,7 +1,6 @@
-var counter = 0;
-var el = document.getElementById('seconds-counter');
-function incrementSeconds() {
-    counter += 1;
-    el.innerText = "You have been here for " + counter + " seconds.";
-}
-var cancel = setInterval(incrementSeconds, 1000);
+const dots = document.querySelectorAll('.grid-item');
+dots.forEach(dot => dot.addEventListener('click', myFunc));
+
+function myFunc(e) {
+    console.log(event.target.style.background);
+  }
