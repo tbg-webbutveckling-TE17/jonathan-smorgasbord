5d88cf82f89827f3d71d4ef7af57cdcf2fa3d87e
jjiojio jiojojoijio <helloantonhowareyou@outlook.com>
2019-10-04 11:24:13 +0200

    Continued jQuery testing

--- projects/jQuery/assets/js/1.js
+++ projects/jQuery/assets/js/1.js
@@ -24,14 +24,67 @@
 //         });
 //     });
 // });
-$(function () {
-    $("#textLoad").click(function (e) {
-        $("#loadText").load("test.html #test", function (response, status, xhr) {
-            if (status == "error") 
-            {
-                $("#loadText").html("Error: " + xhr.status + ": " + xhr.statusText);
-            }
+// $(function () {
+//     $("#textLoad").click(function (e) {
+//         $("#loadText").load("test.html #test", function (response, status, xhr) {
+//             if (status == "error") 
+//             {
+//                 $("#loadText").html("Error: " + xhr.status + ": " + xhr.statusText);
+//             }
 
-        });
-    });
+//         });
+//     });
+// });
+
+//Execsises 5
+
+// $(function()
+// {
+//     $("button").click(function() {
+//         $("#img").animate({
+//             width: "200px"
+//         });
+//     });
+// });
+
+// $(function()
+// {
+//     $("button").click(function() {
+//         $("#img")
+//             .animate({width: "200px"})
+//             .animate({height: "200px"})
+//             .animate({width: "300px"})
+//             .animate({height: "300px"})
+//     });
+// });
+
+// $(function()
+// {
+//     $("button").click(function() {
+//         $("#img")
+//             .animate({width: "+=200px"})
+//             .animate({height: "+=200px"})
+//             .animate({width: "-=300px"})
+//             .animate({height: "-=300px"})
+//     });
+// });
+
+$(function()
+{
+    var slides = $("#slider").children();
+    console.log(slides)
+    x = 1;
+    setInterval(function() {
+        $(slides).hide();
+        $(slides).parents("#slider>li:nth-child(" + x + ")").find("img").show();
+        if (x == slides.length) 
+        {
+            $('#mydiv').children('input').each(function () {
+                alert(this.value); // "this" is the current element in the loop
+            });
+        }
+        else {
+            x++;
+        }
+    }, 2000)
 });
