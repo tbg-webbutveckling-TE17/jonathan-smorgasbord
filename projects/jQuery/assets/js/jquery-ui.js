9ff741681b46fb1e9383e48719991f6c51e5fc68
jjiojio jiojojoijio <helloantonhowareyou@outlook.com>
2019-10-25 11:20:02 +0200

    Continued with jQueryUI

--- projects/jQuery/assets/js/jquery-ui.js
+++ projects/jQuery/assets/js/jquery-ui.js
@@ -5,3 +5,27 @@ console.log(string);
 $(function() {
     $("#accordion").accordion()
 });
+
+$(function() {
+    var allTags = [
+        "ASD",
+        "BdD",
+        "GGG",
+        "GOOO",
+        "WOOO"
+    ]
+    $(tag).autocomplete({
+        source: allTags
+    });
+});
+
+$(function() {
+    $("#progress").progressbar({
+        value: 50
+    });
+});
+
+$( function() {
+    $( document ).tooltip();
+  } );
+
