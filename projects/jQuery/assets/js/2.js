55002b04e2b769658a24c859b8fa9f6f062ea396
jjiojio jiojojoijio <helloantonhowareyou@outlook.com>
2019-12-13 11:24:53 +0100

    continued on paperjs

---
+++ projects/jQuery/assets/js/2.js
@@ -0,0 +1,10 @@
+path = new Path.Circle(100, 100, 30);
+path.fillColor = "white";
+
+var circleArr = [];
+circleArr.push(path);
+console.log(circleArr[0]);
+
+function onFrame() {
+    for (i = 0; i < circleArr.length; i++)
+}
