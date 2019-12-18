aadbda72de8a07f31d75eb29f5073e4d256af801
jjiojio jiojojoijio <helloantonhowareyou@outlook.com>
2019-12-18 12:38:41 +0100

    kinda works now i think

--- projects/jQuery/assets/js/2.js
+++ projects/jQuery/assets/js/2.js
@@ -1,10 +1,25 @@
-path = new Path.Circle(100, 100, 30);
-path.fillColor = "white";
-
 var circleArr = [];
-circleArr.push(path);
-console.log(circleArr[0]);
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        var maxPoint = new Point(view.size.width, view.size.height);
+        var randomPoint = Point.random();
+        var point = maxPoint * randomPoint;
+        var newCircle = new Path.Circle(new Point(point), 800);
+        newCircle.fillColor = "blue";
+        circleArr.push(newCircle);
+    }
+    console.log(circleArr);
+});
 
 function onFrame() {
-    for (i = 0; i < circleArr.length; i++)
+    for (i = 0; i < circleArr.length; i++) {
+        circleArr[i].scale(0.96)
+        circleArr[i].fillColor.hue += 3;
+        
+        if (circleArr[i].area < 1) {
+            circleArr[i].remove()
+            circleArr.splice(i, 1);
+        }
+    }
 }
