7289a356cc7f427f2238952e7a52843ae2f968c8
OverRibban <44964151+OverRibban@users.noreply.github.com>
2020-03-31 13:59:11 +0200

    started coding

---
+++ projects/computerStore/index.js
@@ -0,0 +1,9 @@
+var productButton = document.querySelectorAll(".buyButton");
+
+productButton.forEach(btn => {
+    btn.addEventListener("click", function() {
+        console.log(event.target.);
+    });
+});
+
+console.log("hello");
